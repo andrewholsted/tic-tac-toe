d4ede3af0c0ec172bb3607ad6f754ad6be9c3516
FajitaNachos <andrew@fajitanachos.com>
2013-09-16 23:53:32 +0900

    Clean up a few things

--- js/index.js
+++ js/index.js
@@ -6,47 +6,48 @@ $(document).ready(function(){
   }
 
   Game.prototype = {
+
     setup: function(options){
       this.currentPlayer = options.player;
       this.computer = options.computer;
-      this.board.draw(options.size);
+      this.board.draw();
     },
 
     step: function(){
-      // check to see if we have a winner
+
+      // check for winner
       var winner = this.board.hasWinner();
       if (winner === 'X' || winner == 'O') {
         alert (winner + ' wins');
         game.active = false;
       }
 
-      // no winner so check to see if board is full
+      // no winner, check if board is full
       else if (this.board.isFull()) { 
         alert('it\'s a tie..');
       }
 
-      // board isn't full so the game continues
+      // board isn't full, carry on
       else { 
 
         // if the last player to move was X, make it O and vice versa
         game.currentPlayer = (game.currentPlayer === 'X') ? 'O' : 'X';
 
-        //if new player is the computer, let's find the best move
+        // check if it's the computers turn
         if (game.currentPlayer === game.computer) {
 
-          //next best move
+          // find the computer's best move with Negamax
           var move = this.board.solver.solve(this.board, game.currentPlayer);
           
-          //let's move there behind the scenes
           this.board.move(move, game.currentPlayer);
 
-          //and draw it to the canvas
+          // draw the move on the canvas
           this.board.squares[move].draw(game.currentPlayer);
       
-          //step forward
+          // step it up
           this.step();
         }
-    }
+      }
     }
   }
 
@@ -54,21 +55,18 @@ $(document).ready(function(){
 
     this.solver = new Negamax();
 
-    //simple board to work with behind the scenes
     this.board = [0,0,0,0,0,0,0,0,0];
-
-    //setup our canvas squares
     this.squares = [];
     this.canvas = $('#board');
 
-    //possible winning combinations
+    // all possible winning combinations
     this.wins = [
       [0,1,2],[3,4,5],[6,7,8],
       [0,3,6],[1,4,7],[2,5,8],
       [0,4,8],[2,4,6]
     ];
 
-    // this if for when we load the new board in the Negamax
+    // this is only used when we load the board during our Negamax calls
     if (arguments.length > 0) {
       for (var i = 0; i < 9; i++) {
         this.board[i] = arguments[0][i];
@@ -76,64 +74,67 @@ $(document).ready(function(){
     }
   }
 
+
   Board.prototype = {
 
-    //draw the initial board
+    //draw the board
     draw: function(){
-      var canvas = this.canvas;
-      var context = canvas[0].getContext('2d');
-      var width = canvas.width();
+
+      var context = this.canvas[0].getContext('2d');
+      var width = this.canvas.width();
+      var oneThird = width/3;
+      var twoThirds = (width*2)/3;
+      
       context.beginPath();
 
-      // draw vertical lines
-      context.moveTo(width/3,0);     //width * .3  , 0
-      context.lineTo(width/3,width);   //width * .3 , width
-      context.moveTo((width*2)/3,0);    //width * .6 , 0
-      context.lineTo((width*2)/3,width);  // width *.6, width
+      // draw vertical lines relative to canvas size
+      context.moveTo(oneThird,0);     
+      context.lineTo(oneThird,width); 
 
-      // draw horizontal lines
-      context.moveTo(0,width/3);     //0, width * .3
-      context.lineTo(width,width/3);   // width, width *.3
-      context.moveTo(0,(width*2)/3);    //0 , width *.6
-      context.lineTo(width,(width*2)/3);  //width, width *.6
+      context.moveTo(twoThirds,0);   
+      context.lineTo(twoThirds,width); 
+
+      // draw horizontal lines relative to canvas size
+      context.moveTo(0,oneThird);     
+      context.lineTo(width,oneThird);
+
+      context.moveTo(0,twoThirds);   
+      context.lineTo(width,twoThirds);
+
       context.stroke();
 
       var id = 0;
-      var width = width/3;
-      var height = width;
       for(i=0;i<=2;i++){
         for(j=0;j<=2;j++){
-          this.squares.push(new Square(id++,width*j, height*i, canvas));
+          this.squares.push(new Square(id++,oneThird*j, oneThird*i, this.canvas));  // (id, width, height, canvas)
         }
       }
 
-       this.listen();
+      // attach our canvas event listener
+      this.listen();
     },
 
-    // setup out click listener for the board
     listen: function(callback){
-
-      var squares = this.squares;
       var that = this;
-
+      var squares = that.squares;
+      
       this.canvas.on('click', function(event){
         
-        // allow clicks if the game is active
         if(game.active){
 
-          //get the position of our mouse click
+          //get the x,y position of our mouse click relative to the canvas
           var offset = $(this).offset();
           x = event.pageX - offset.left;
           y = event.pageY - offset.top;
 
-          // loop through our squares and see which one we clicked on
+          // find which square we clicked on
           $.each(squares, function(){
-            var id = this.check_position(x,y);
+            var id = this.checkPosition(x,y);
             if(id >= 0){
-              // check that the square is empty
+              // make sure it's empty square is empty
               if(that.board[id] === 0){
 
-                // make the move, draw it on the board, and step the game
+                // make the move, draw it on the canvas, and step the game
                 that.move(id, game.currentPlayer);
                 this.draw(game.currentPlayer);
                 game.step();
@@ -146,19 +147,17 @@ $(document).ready(function(){
 
     getMoves: function() {
       var moves = [];
-      for (var i = 0; i < 9; i++) {
-        if (this.board[i] === 0) {
+      for (var i = 0; i < this.board.length; i++) {
+        if (this.board[i] === 0)
           moves.push(i);
-        }
       }
       return moves;
     },
 
     isFull: function() {
-      for(var i = 0; i < 9; i++) {
-        if (this.board[i] === 0) {
+      for(var i = 0; i < this.board.length; i++) {
+        if (this.board[i] === 0)
           return false;
-        }
       }
       return true;
     },
@@ -174,6 +173,7 @@ $(document).ready(function(){
     copy: function(){
       return new Board(this.board);
     },
+
     hasWinner: function() {
       this.w = this.wins;
       this.s = this.getSquare;
@@ -181,8 +181,7 @@ $(document).ready(function(){
         if (this.s(this.w[i][0]) === this.s(this.w[i][1]) 
           && this.s(this.w[i][0]) === this.s(this.w[i][2]) 
           && this.s(this.w[i][0]) != 0) {
-          return this.s(this.w[i][0]);
-
+            return this.s(this.w[i][0]);
         }
       }
       return -1;
@@ -190,59 +189,65 @@ $(document).ready(function(){
   };
 
   function Square(id,offsetX, offsetY, canvas) {
-      this.id = id;
-      this.value = null;
-      this.squareWidth = canvas.width()/3;
-      this.canvas = canvas;
-      this.xMin = offsetX;
-      this.yMin = offsetY;
+    this.id = id;
+    this.width = canvas.width()/3;
+    this.context = canvas[0].getContext('2d');
+    this.xMin = offsetX;
+    this.yMin = offsetY;
   }
 
   Square.prototype = {
+
     draw: function(shape){
+      var w = this.width;
       var xMin = this.xMin;
       var yMin = this.yMin;
-      var maxOffset = (this.squareWidth*.85);
-      var minOffset = (this.squareWidth*.15);
-      var context = this.canvas[0].getContext('2d');
+      var maxOffset = (w*.85);
+      var minOffset = (w*.15);
+      var context = this.context;
       if (shape == 'X') {
+        //draw X
+
         context.beginPath();
-        //draw first diagonal
+       
         context.moveTo(xMin+minOffset,yMin+minOffset);
         context.lineTo(xMin+maxOffset,yMin+maxOffset);
 
-        //draw second diagonal
         context.moveTo(xMin+maxOffset,yMin+minOffset);
         context.lineTo(xMin+minOffset,yMin+maxOffset);
         
+        context.strokeStyle = '#777777';
         context.stroke();
-        this.empty = false;
-        this.value = 'X';
+
       }
+
       else if (shape == 'O'){
-        //I can draw circles
+
+        // draw O
+
         context.beginPath();
-        context.arc(xMin+(this.squareWidth/2),yMin+(this.squareWidth/2),this.squareWidth/2.6,this.squareWidth/2.6,Math.PI*2,true);
+
+        context.arc(xMin+(w/2),yMin+(w/2),w/2.6,w/2.6,Math.PI*2,true);
+
+        context.strokeStyle = '#777777';
         context.stroke();
-        this.value = 'O';
       }
     },
 
-    check_position: function(x,y){
+    checkPosition: function(x,y){
       //get which square we clicked on
-      var xMax = this.xMin + (this.squareWidth-1);
-      var yMax = this.yMin + (this.squareWidth-1);
+      var xMax = this.xMin + (this.width-1);
+      var yMax = this.yMin + (this.width-1);
       if ((x >= this.xMin && x <= xMax) && (y >= this.yMin && y <= yMax)){
         return this.id;
       }
     }
   }
 
-  // Negamus Maximus
+  // Negamax
+  // Inspired by http://mkuklis.github.io/tictactoe/docs/tictactoe.html
 
-  //Inspired by http://mkuklis.github.io/tictactoe/docs/tictactoe.html
-
-var Negamax = function(maxDepth) {
+  var Negamax = function(maxDepth) {
     this.INFINITY = 999,
     this.maxDepth = maxDepth,
     this.bestmove = -1;
@@ -324,15 +329,17 @@ var Negamax = function(maxDepth) {
   });
 
   $('#new-game').on('click', function(){
+    var size = $('#board').width();
     $('#board').remove();
-    $('#container').prepend($("<canvas id='board' width='150' height='150'></canvas>"))
+    $('#container').prepend($("<canvas id='board' width='"+size+"' height='"+size+"'></canvas>"))
+    
     game = new Game();
     game.setup({
       computer:'O',
       player: 'X',
       size: 3,
     });
-
+    
   });
 });
 
